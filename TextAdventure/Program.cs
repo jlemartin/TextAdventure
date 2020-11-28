b15bf580c706fee21492d95c78578d3b2374b174
John Martin <jlemartin7@gmail.com>
2020-11-28 10:15:35 -0600

    started Text Adventure

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "John Doe";
+
+
+            Console.WriteLine("Game Title");
+            Console.WriteLine("Welcome to ...");
+            Console.WriteLine("Please enter your characters's name:");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Great, Your character is now named " + CharacterName);
+
+        }
+    }
+}
